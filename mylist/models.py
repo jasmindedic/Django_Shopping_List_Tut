b3419b22b6ba8bd83ccb4d6eafa180b236b6a27f
jasmindedic <jdedic1999@gmail.com>
2024-03-05 20:42:07 +0100

    Finished Tutorial

---
+++ mylist/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import date
+
+# Create your models here.
+class ShoppingItem(models.Model):
+    created_at = models.DateField(default=date.today)
+    name = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
+
